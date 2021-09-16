ea806fd6f89c463f5a3916eb899725d110cf2fe2
Luca Scalzotto <luca@scorpiac.com>
2021-09-16 23:00:10 +0200

    Set up SI units

---
+++ src/services/DistanceConverter.ts
@@ -0,0 +1,11 @@
+// Speed of light in a vacuum, in meters per second.
+const LIGHT_IN_VACUUM = 299_792_458;
+
+export class DistanceConverter {
+  public static readonly NAME = 'DistanceConverter';
+
+  public convertDistance(fromValue: number, fromUnit: string, toUnit: string): number {
+    // TODO
+    return 0;
+  }
+}
---
+++ src/services/SI.ts
@@ -0,0 +1,36 @@
+// Source: https://physics.nist.gov/cuu/Units/prefixes.html
+
+export const SI_UNITS: Record<string, SIUnit> = {
+  yocto: siUnit('y', -24),
+  zepto: siUnit('z', -21),
+  atto: siUnit('a', -18),
+  femto: siUnit('f', -15),
+  pico: siUnit('p', -12),
+  nano: siUnit('n', -9),
+  micro: siUnit('µ', -6),
+  milli: siUnit('m', -3),
+  centi: siUnit('c', -2),
+  deci: siUnit('d', -1),
+  deka: siUnit('da', 1),
+  hecto: siUnit('h', 2),
+  kilo: siUnit('k', 3),
+  mega: siUnit('M', 6),
+  giga: siUnit('G', 9),
+  tera: siUnit('T', 12),
+  peta: siUnit('P', 15),
+  exa: siUnit('E', 18),
+  zetta: siUnit('Z', 21),
+  yotta: siUnit('Y', 24),
+};
+
+interface SIUnit {
+  symbol: string;
+  factor: number;
+}
+
+function siUnit(symbol: string, factor: number): SIUnit {
+  return {
+    symbol,
+    factor,
+  };
+}
