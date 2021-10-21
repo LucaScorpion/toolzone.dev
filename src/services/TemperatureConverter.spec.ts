21c7504d183327403881cc250fc3acc455812c79
Luca Scalzotto <luca@scorpiac.com>
2021-10-21 23:54:41 +0200

    Add temperature conversion

---
+++ src/services/TemperatureConverter.spec.ts
@@ -0,0 +1,21 @@
+import { Injector } from './Injector';
+import { TemperatureConverter, TemperatureUnit } from './TemperatureConverter';
+
+describe('TemperatureConverter', () => {
+  const converter = new Injector().resolve(TemperatureConverter);
+
+  it.each`
+    value    | from                          | to                            | expected
+    ${12.34} | ${TemperatureUnit.kelvin}     | ${TemperatureUnit.kelvin}     | ${12.34}
+    ${12.34} | ${TemperatureUnit.celsius}    | ${TemperatureUnit.celsius}    | ${12.34}
+    ${12.34} | ${TemperatureUnit.fahrenheit} | ${TemperatureUnit.fahrenheit} | ${12.34}
+    ${12.34} | ${TemperatureUnit.kelvin}     | ${TemperatureUnit.celsius}    | ${-260.81}
+    ${12.34} | ${TemperatureUnit.celsius}    | ${TemperatureUnit.kelvin}     | ${285.49}
+    ${12.34} | ${TemperatureUnit.kelvin}     | ${TemperatureUnit.fahrenheit} | ${-437.46}
+    ${12.34} | ${TemperatureUnit.fahrenheit} | ${TemperatureUnit.kelvin}     | ${262.23}
+    ${12.34} | ${TemperatureUnit.celsius}    | ${TemperatureUnit.fahrenheit} | ${54.21}
+    ${12.34} | ${TemperatureUnit.fahrenheit} | ${TemperatureUnit.celsius}    | ${-10.92}
+  `('converts the temperature', ({ value, from, to, expected }) => {
+    expect(converter.convert(value, from, to)).toBeCloseTo(expected);
+  });
+});
---
+++ src/services/TemperatureConverter.ts
@@ -0,0 +1,58 @@
+export enum TemperatureUnit {
+  kelvin,
+  celsius,
+  fahrenheit,
+}
+
+export class TemperatureConverter {
+  public static readonly NAME = 'TemperatureConverter';
+
+  public convert(value: number, fromUnit: TemperatureUnit, toUnit: TemperatureUnit): number {
+    const kelvinValue = new unitMap[fromUnit](value).toKelvin();
+    return unitMap[toUnit].fromKelvin(kelvinValue);
+  }
+}
+
+const KELVIN_DIFF = 273.15;
+
+abstract class BaseTemperatureUnit {
+  public constructor(protected readonly value: number) {}
+
+  public abstract toKelvin(): number;
+}
+
+class Kelvin extends BaseTemperatureUnit {
+  public static fromKelvin(kelvin: number): number {
+    return kelvin;
+  }
+
+  public toKelvin(): number {
+    return this.value;
+  }
+}
+
+class Celsius extends BaseTemperatureUnit {
+  public static fromKelvin(kelvin: number): number {
+    return kelvin - KELVIN_DIFF;
+  }
+
+  public toKelvin(): number {
+    return this.value + KELVIN_DIFF;
+  }
+}
+
+class Fahrenheit extends BaseTemperatureUnit {
+  public static fromKelvin(kelvin: number): number {
+    return (kelvin - KELVIN_DIFF) * 1.8 + 32;
+  }
+
+  public toKelvin(): number {
+    return (this.value - 32) / 1.8 + KELVIN_DIFF;
+  }
+}
+
+const unitMap = {
+  [TemperatureUnit.kelvin]: Kelvin,
+  [TemperatureUnit.celsius]: Celsius,
+  [TemperatureUnit.fahrenheit]: Fahrenheit,
+};
