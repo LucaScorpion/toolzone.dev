b3490fb8e7031075d63f31263907c25b2f055898
Luca Scalzotto <luca@scorpiac.com>
2021-10-22 01:51:03 +0200

    Add GitHub octo corner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { classNames } from '../utils/classNames';
+import { GitHubCorner } from './GitHubCorner';
 
 export interface Props {
   className?: string;
@@ -8,8 +9,9 @@ export interface Props {
 export const Layout: React.FC<Props> = ({ children, className }) => (
   <div className={classNames('layout', className)}>
     <div className="top-bar">
-      <h1>Utilitools</h1>
+      <h1 className="title">Utilitools</h1>
     </div>
+    <GitHubCorner />
     <main>{children}</main>
   </div>
 );
