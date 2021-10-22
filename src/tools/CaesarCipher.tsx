33bcdabb18281f5b7fb6d0fa2d4d268083bd707a
Luca Scalzotto <luca@scorpiac.com>
2021-10-23 01:03:12 +0200

    Add shift option to caesar cipher

--- src/tools/CaesarCipher.tsx
+++ src/tools/CaesarCipher.tsx
@@ -3,6 +3,7 @@ import { useService } from '../hooks/useService';
 import { useEffect, useState } from 'react';
 import { TextAreaInput } from '../components/TextAreaInput';
 import { CaesarCipherService } from '../services/CaesarCipherService';
+import { NumberInput } from '../components/NumberInput';
 
 export const CaesarCipher: Tool = {
   name: 'Caesar cipher',
@@ -10,19 +11,29 @@ export const CaesarCipher: Tool = {
     const service = useService(CaesarCipherService);
     const [value, setValue] = useState('');
     const [result, setResult] = useState('');
+    const [shift, setShift] = useState(13);
 
     useEffect(() => {
       try {
-        setResult(service.encode(value, 13));
+        setResult(service.encode(value, shift));
       } catch (e) {
         setResult(e.message);
       }
-    }, [service, value]);
+    }, [service, value, shift]);
 
     return (
-      <div className="panels">
-        <TextAreaInput value={value} onChange={setValue} placeholder="Hello world!" />
-        <TextAreaInput value={result} onChange={setResult} placeholder="Uryyb jbeyq!" />
+      <div className="panels-options">
+        <div className="panels">
+          <TextAreaInput value={value} onChange={setValue} placeholder="Hello world!" />
+          <TextAreaInput value={result} onChange={setResult} placeholder="Uryyb jbeyq!" />
+        </div>
+        <div className="options">
+          <div>
+            <div>
+              Shift: <NumberInput value={shift} onChange={setShift} min={1} max={25} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   },
