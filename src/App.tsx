8b6906ce9868c20886e913b5e5306ae0d5870a49
Luca Scalzotto <luca@scorpiac.com>
2022-09-17 14:57:32 +0200

    Simplify routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ToolsPage } from './pages/ToolsPage';
 
 export const App: React.FC = () => (
   <Routes>
     <Route path="/tools/:tool" element={<ToolsPage />} />
-    <Route path="/tools" element={<ToolsPage />} />
-    <Route path="*" element={<Navigate to="/tools" />} />
+    <Route path="*" element={<ToolsPage />} />
   </Routes>
 );
